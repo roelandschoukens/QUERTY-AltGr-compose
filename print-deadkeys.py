765a10b77f498522034fe8df252799683d30d972
Roeland Schoukens <roeland.schoukens@gmail.com>
2019-03-31 23:04:34 +1300

    :beetle: version 1.8.2 — Use spacing accents for dead keys…
    because getting non-spacing combining marks is actually quite annoying.

--- print-deadkeys.py
+++ print-deadkeys.py
@@ -11,21 +11,22 @@ latin = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 greek = "ΑΒΧΔΕΦΓΗΙϑΚΛΜΝΟΠΘΡΣΤΥςΩΞΨΖαβχδεφγηιϕκλμνοπθρστυϖωξψζ"
 
 class DeadKey:
-    def __init__(self, name, keypress, deadkey, extras):
+    def __init__(self, name, keypress, deadkey, combining_accent, extras):
         self.name = name
         self.keypress = keypress
         self.deadkey = deadkey
+        self.combining_accent = combining_accent
         self.extras = extras
 
     def isaccent(self):
-        return unicodedata.combining(self.deadkey) != 0
+        return unicodedata.combining(self.combining_accent) != 0
         
     def all(self):
         """ all combinations"""
         yield from self.extras
         if self.isaccent():
             for l in latin:
-                a = l + self.deadkey
+                a = l + self.combining_accent
                 b = unicodedata.normalize("NFC", a)
                 if len(b) == 1:
                     yield (l, b)
@@ -34,13 +35,13 @@ class DeadKey:
 # Note that not all characters work as dead keys. En-dash doesn't work.
 # luckily all our combining diacritics work.
 dk = [
-    DeadKey("Backtick",  "`", "\u0300", []), 
-    DeadKey("Quote",      "'", "\u0301", [
+    DeadKey("Backtick",  "`", "`", "\u0300", []), 
+    DeadKey("Quote",      "'", "´", "\u0301", [
         (",", "‚")  # ← that's a low quotation mark
     ]),
-    DeadKey("Circumflex", "6", "\u0302", []), 
-    DeadKey("Tilde",      "Shift + `", "\u0303", []), 
-    DeadKey("Dash",     "-", "\u0304", [
+    DeadKey("Circumflex", "6", "^", "\u0302", []), 
+    DeadKey("Tilde",      "Shift + `", "~", "\u0303", []), 
+    DeadKey("Dash",     "-", "¯", "\u0304", [
         (">", "→"),
         ("<", "←"),
         ("v", "↓"),
@@ -50,18 +51,18 @@ dk = [
         (":", "÷"),
         ("+", "±"),
     ]), 
-    DeadKey("Dot",  ".", "\u0307", [
+    DeadKey("Dot",  ".", "˙", "\u0307", [
         (".", "…"),
         ("-", "·"),
         ("=", "•"),
     ]), 
-    DeadKey("Double quote",  "Shift + '", "\u0308", [
+    DeadKey("Double quote",  "Shift + '", "¨", "\u0308", [
         (",", "„")
         ]), 
-    DeadKey("Ring above", "o", "\u030a", []), 
-    DeadKey("Caron",      "Shift + 6", "\u030c", []),
-    DeadKey("Cedilla",    ",", "\u0327", []),
-    DeadKey("Mathematical characters",          "+", "×",      [
+    DeadKey("Ring above", "o", "˚", "\u030a", []), 
+    DeadKey("Caron",      "Shift + 6", "ˇ", "\u030c", []),
+    DeadKey("Cedilla",    ",", "¸", "\u0327", []),
+    DeadKey("Mathematical characters",          "+", "×", "×",      [
         ("-", "−"), # minus sign (subtly different from en dash)
         ("<", "≤"),
         (">", "≥"),
@@ -78,7 +79,7 @@ dk = [
         ("S", "∑"),
         ("P", "∏"),
     ]),
-    DeadKey("Greek", "g", "γ", list(zip(latin, greek))),
+    DeadKey("Greek", "g", "γ", "γ", list(zip(latin, greek))),
 ]
 
 what = sys.argv[1] if len(sys.argv) == 2 else ""
