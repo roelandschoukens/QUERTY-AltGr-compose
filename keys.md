d47b6de80f575b006825408595fe6918f5be8676
Roeland Schoukens <roeland.schoukens@gmail.com>
2019-06-11 22:26:41 +1200

    :hammer: The python file will now update the .klc and .md file all by itself.

--- keys.md
+++ keys.md
@@ -3,106 +3,107 @@
 ## Combinations with dead keys
 
 ### Quote: <kbd>'</kbd>
-| base | char |
-| ---- | ---- |
-| <kbd>,</kbd>  | ‚ |
+| base | char | name     |
+| ---- | ---- | -------- |
+| <kbd>,</kbd>  | ‚ | single low-9 quotation mark |
 
 ### Dash: <kbd>-</kbd>
-| base | char |
-| ---- | ---- |
-| <kbd>></kbd>  | → |
-| <kbd><</kbd>  | ← |
-| <kbd>v</kbd>  | ↓ |
-| <kbd>^</kbd>  | ↑ |
-| <kbd>-</kbd>  | – |
-| <kbd>_</kbd>  | — |
-| <kbd>:</kbd>  | ÷ |
-| <kbd>+</kbd>  | ± |
+| base | char | name     |
+| ---- | ---- | -------- |
+| <kbd>></kbd>  | → | rightwards arrow |
+| <kbd><</kbd>  | ← | leftwards arrow |
+| <kbd>v</kbd>  | ↓ | downwards arrow |
+| <kbd>^</kbd>  | ↑ | upwards arrow |
+| <kbd>-</kbd>  | – | en dash |
+| <kbd>_</kbd>  | — | em dash |
+| <kbd>:</kbd>  | ÷ | division sign |
+| <kbd>+</kbd>  | ± | plus-minus sign |
 
 ### Dot: <kbd>.</kbd>
-| base | char |
-| ---- | ---- |
-| <kbd>.</kbd>  | … |
-| <kbd>-</kbd>  | · |
-| <kbd>=</kbd>  | • |
+| base | char | name     |
+| ---- | ---- | -------- |
+| <kbd>.</kbd>  | … | horizontal ellipsis |
+| <kbd>-</kbd>  | · | middle dot |
+| <kbd>=</kbd>  | • | bullet |
 
 ### Double quote: <kbd>Shift + '</kbd>
-| base | char |
-| ---- | ---- |
-| <kbd>,</kbd>  | „ |
+| base | char | name     |
+| ---- | ---- | -------- |
+| <kbd>,</kbd>  | „ | double low-9 quotation mark |
 
 ### Mathematical characters: <kbd>+</kbd>
-| base | char |
-| ---- | ---- |
-| <kbd>-</kbd>  | − |
-| <kbd><</kbd>  | ≤ |
-| <kbd>></kbd>  | ≥ |
-| <kbd>/</kbd>  | ≠ |
-| <kbd>~</kbd>  | ≈ |
-| <kbd>v</kbd>  | √ |
-| <kbd>o</kbd>  | ° |
-| <kbd>'</kbd>  | ′ |
-| <kbd>"</kbd>  | ″ |
-| <kbd>8</kbd>  | ∞ |
-| <kbd>D</kbd>  | ∆ |
-| <kbd>d</kbd>  | ∂ |
-| <kbd>s</kbd>  | ∫ |
-| <kbd>S</kbd>  | ∑ |
-| <kbd>P</kbd>  | ∏ |
+| base | char | name     |
+| ---- | ---- | -------- |
+| <kbd>-</kbd>  | − | minus sign |
+| <kbd>.</kbd>  | ⋅ | dot operator |
+| <kbd><</kbd>  | ≤ | less-than or equal to |
+| <kbd>></kbd>  | ≥ | greater-than or equal to |
+| <kbd>/</kbd>  | ≠ | not equal to |
+| <kbd>~</kbd>  | ≈ | almost equal to |
+| <kbd>v</kbd>  | √ | square root |
+| <kbd>o</kbd>  | ° | degree sign |
+| <kbd>'</kbd>  | ′ | prime |
+| <kbd>"</kbd>  | ″ | double prime |
+| <kbd>8</kbd>  | ∞ | infinity |
+| <kbd>D</kbd>  | ∆ | increment |
+| <kbd>d</kbd>  | ∂ | partial differential |
+| <kbd>s</kbd>  | ∫ | integral |
+| <kbd>S</kbd>  | ∑ | n-ary summation |
+| <kbd>P</kbd>  | ∏ | n-ary product |
 
 ### Greek: <kbd>g</kbd>
-| base | char |
-| ---- | ---- |
-| <kbd>A</kbd>  | Α |
-| <kbd>B</kbd>  | Β |
-| <kbd>C</kbd>  | Χ |
-| <kbd>D</kbd>  | Δ |
-| <kbd>E</kbd>  | Ε |
-| <kbd>F</kbd>  | Φ |
-| <kbd>G</kbd>  | Γ |
-| <kbd>H</kbd>  | Η |
-| <kbd>I</kbd>  | Ι |
-| <kbd>J</kbd>  | ϑ |
-| <kbd>K</kbd>  | Κ |
-| <kbd>L</kbd>  | Λ |
-| <kbd>M</kbd>  | Μ |
-| <kbd>N</kbd>  | Ν |
-| <kbd>O</kbd>  | Ο |
-| <kbd>P</kbd>  | Π |
-| <kbd>Q</kbd>  | Θ |
-| <kbd>R</kbd>  | Ρ |
-| <kbd>S</kbd>  | Σ |
-| <kbd>T</kbd>  | Τ |
-| <kbd>U</kbd>  | Υ |
-| <kbd>V</kbd>  | ς |
-| <kbd>W</kbd>  | Ω |
-| <kbd>X</kbd>  | Ξ |
-| <kbd>Y</kbd>  | Ψ |
-| <kbd>Z</kbd>  | Ζ |
-| <kbd>a</kbd>  | α |
-| <kbd>b</kbd>  | β |
-| <kbd>c</kbd>  | χ |
-| <kbd>d</kbd>  | δ |
-| <kbd>e</kbd>  | ε |
-| <kbd>f</kbd>  | φ |
-| <kbd>g</kbd>  | γ |
-| <kbd>h</kbd>  | η |
-| <kbd>i</kbd>  | ι |
-| <kbd>j</kbd>  | ϕ |
-| <kbd>k</kbd>  | κ |
-| <kbd>l</kbd>  | λ |
-| <kbd>m</kbd>  | μ |
-| <kbd>n</kbd>  | ν |
-| <kbd>o</kbd>  | ο |
-| <kbd>p</kbd>  | π |
-| <kbd>q</kbd>  | θ |
-| <kbd>r</kbd>  | ρ |
-| <kbd>s</kbd>  | σ |
-| <kbd>t</kbd>  | τ |
-| <kbd>u</kbd>  | υ |
-| <kbd>v</kbd>  | ϖ |
-| <kbd>w</kbd>  | ω |
-| <kbd>x</kbd>  | ξ |
-| <kbd>y</kbd>  | ψ |
-| <kbd>z</kbd>  | ζ |
+| base | char | name     |
+| ---- | ---- | -------- |
+| <kbd>A</kbd>  | Α | greek capital letter alpha |
+| <kbd>B</kbd>  | Β | greek capital letter beta |
+| <kbd>C</kbd>  | Χ | greek capital letter chi |
+| <kbd>D</kbd>  | Δ | greek capital letter delta |
+| <kbd>E</kbd>  | Ε | greek capital letter epsilon |
+| <kbd>F</kbd>  | Φ | greek capital letter phi |
+| <kbd>G</kbd>  | Γ | greek capital letter gamma |
+| <kbd>H</kbd>  | Η | greek capital letter eta |
+| <kbd>I</kbd>  | Ι | greek capital letter iota |
+| <kbd>J</kbd>  | ϑ | greek theta symbol |
+| <kbd>K</kbd>  | Κ | greek capital letter kappa |
+| <kbd>L</kbd>  | Λ | greek capital letter lamda |
+| <kbd>M</kbd>  | Μ | greek capital letter mu |
+| <kbd>N</kbd>  | Ν | greek capital letter nu |
+| <kbd>O</kbd>  | Ο | greek capital letter omicron |
+| <kbd>P</kbd>  | Π | greek capital letter pi |
+| <kbd>Q</kbd>  | Θ | greek capital letter theta |
+| <kbd>R</kbd>  | Ρ | greek capital letter rho |
+| <kbd>S</kbd>  | Σ | greek capital letter sigma |
+| <kbd>T</kbd>  | Τ | greek capital letter tau |
+| <kbd>U</kbd>  | Υ | greek capital letter upsilon |
+| <kbd>V</kbd>  | ς | greek small letter final sigma |
+| <kbd>W</kbd>  | Ω | greek capital letter omega |
+| <kbd>X</kbd>  | Ξ | greek capital letter xi |
+| <kbd>Y</kbd>  | Ψ | greek capital letter psi |
+| <kbd>Z</kbd>  | Ζ | greek capital letter zeta |
+| <kbd>a</kbd>  | α | greek small letter alpha |
+| <kbd>b</kbd>  | β | greek small letter beta |
+| <kbd>c</kbd>  | χ | greek small letter chi |
+| <kbd>d</kbd>  | δ | greek small letter delta |
+| <kbd>e</kbd>  | ε | greek small letter epsilon |
+| <kbd>f</kbd>  | φ | greek small letter phi |
+| <kbd>g</kbd>  | γ | greek small letter gamma |
+| <kbd>h</kbd>  | η | greek small letter eta |
+| <kbd>i</kbd>  | ι | greek small letter iota |
+| <kbd>j</kbd>  | ϕ | greek phi symbol |
+| <kbd>k</kbd>  | κ | greek small letter kappa |
+| <kbd>l</kbd>  | λ | greek small letter lamda |
+| <kbd>m</kbd>  | μ | greek small letter mu |
+| <kbd>n</kbd>  | ν | greek small letter nu |
+| <kbd>o</kbd>  | ο | greek small letter omicron |
+| <kbd>p</kbd>  | π | greek small letter pi |
+| <kbd>q</kbd>  | θ | greek small letter theta |
+| <kbd>r</kbd>  | ρ | greek small letter rho |
+| <kbd>s</kbd>  | σ | greek small letter sigma |
+| <kbd>t</kbd>  | τ | greek small letter tau |
+| <kbd>u</kbd>  | υ | greek small letter upsilon |
+| <kbd>v</kbd>  | ϖ | greek pi symbol |
+| <kbd>w</kbd>  | ω | greek small letter omega |
+| <kbd>x</kbd>  | ξ | greek small letter xi |
+| <kbd>y</kbd>  | ψ | greek small letter psi |
+| <kbd>z</kbd>  | ζ | greek small letter zeta |
 
--- print-deadkeys.py
+++ print-deadkeys.py
@@ -2,6 +2,9 @@
 
 # hint: use set PYTHONIOENCODING=UTF-8 to control encoding of stdout
 
+version = [1, 8, 3]
+
+import re
 import sys
 import unicodedata
 
@@ -11,15 +14,18 @@ latin = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 greek = "ΑΒΧΔΕΦΓΗΙϑΚΛΜΝΟΠΘΡΣΤΥςΩΞΨΖαβχδεφγηιϕκλμνοπθρστυϖωξψζ"
 
 class DeadKey:
-    def __init__(self, name, keypress, deadkey, combining_accent, extras):
+    def __init__(self, name, keypress, basekey, deadkey, combining_accent, extras):
         self.name = name
         self.keypress = keypress
+        self.basekey = basekey
         self.deadkey = deadkey
         self.combining_accent = combining_accent
         self.extras = extras
+        if self.combining_accent and not unicodedata.combining(self.combining_accent):
+            raise ValueError("That is not a combining accent: {}".format(self.combining_accent))
 
     def isaccent(self):
-        return unicodedata.combining(self.combining_accent) != 0
+        return self.combining_accent is not None
         
     def all(self):
         """ all combinations"""
@@ -33,15 +39,14 @@ class DeadKey:
         yield (" ", self.deadkey)
         
 # Note that not all characters work as dead keys. En-dash doesn't work.
-# luckily all our combining diacritics work.
 dk = [
-    DeadKey("Backtick",  "`", "`", "\u0300", []), 
-    DeadKey("Quote",      "'", "´", "\u0301", [
+    DeadKey("Backtick",  "`", "`", "`", "\u0300", []), 
+    DeadKey("Quote",      "'", "'", "´", "\u0301", [
         (",", "‚")  # ← that's a low quotation mark
     ]),
-    DeadKey("Circumflex", "6", "^", "\u0302", []), 
-    DeadKey("Tilde",      "Shift + `", "~", "\u0303", []), 
-    DeadKey("Dash",     "-", "¯", "\u0304", [
+    DeadKey("Circumflex", "6", "6", "^", "\u0302", []), 
+    DeadKey("Tilde",      "Shift + `", "~", "~", "\u0303", []), 
+    DeadKey("Dash",     "-", "-", "¯", "\u0304", [
         (">", "→"),
         ("<", "←"),
         ("v", "↓"),
@@ -51,19 +56,20 @@ dk = [
         (":", "÷"),
         ("+", "±"),
     ]), 
-    DeadKey("Dot",  ".", "˙", "\u0307", [
+    DeadKey("Dot",  ".", ".", "˙", "\u0307", [
         (".", "…"),
         ("-", "·"),
         ("=", "•"),
     ]), 
-    DeadKey("Double quote",  "Shift + '", "¨", "\u0308", [
+    DeadKey("Double quote",  "Shift + '", '"', "¨", "\u0308", [
         (",", "„")
         ]), 
-    DeadKey("Ring above", "o", "˚", "\u030a", []), 
-    DeadKey("Caron",      "Shift + 6", "ˇ", "\u030c", []),
-    DeadKey("Cedilla",    ",", "¸", "\u0327", []),
-    DeadKey("Mathematical characters",          "+", "×", "×",      [
+    DeadKey("Ring above", "o", "o", "˚", "\u030a", []), 
+    DeadKey("Caron",      "Shift + 6", "^", "ˇ", "\u030c", []),
+    DeadKey("Cedilla",    ",", ",", "¸", "\u0327", []),
+    DeadKey("Mathematical characters",          "+", "=", "×", None,      [
         ("-", "−"), # minus sign (subtly different from en dash)
+        (".", "⋅"), # dot operator (subtly different from middle dot)
         ("<", "≤"),
         (">", "≥"),
         ("/", "≠"),
@@ -79,27 +85,117 @@ dk = [
         ("S", "∑"),
         ("P", "∏"),
     ]),
-    DeadKey("Greek", "g", "γ", "γ", list(zip(latin, greek))),
+    DeadKey("Greek", "g", "g", "γ", None, list(zip(latin, greek))),
 ]
 
-what = sys.argv[1] if len(sys.argv) == 2 else ""
+dklut = dict((x.basekey, x) for x in dk)
 
-if what == "klc":
+# patch our KLC template
+
+def klc_tochar(s):
+    if s == '-1':
+        return None
+    elif len(s) == 1:
+        return s
+    else:
+        return chr(int(s, 16))
+
+def klc_fromchar(ch, deadkey):
+    if ch is None:
+        return '-1'
+    if deadkey:
+        return "{0:04x}@".format(ord(ch))
+    else:
+        return ch if re.match("[a-zA-Z0-9]", ch) else "{0:04x}".format(ord(ch))
+
+def klc_tocomment(ch):
+    if ch is None:
+        return '<none>'
+    else:
+        return unicodedata.name(ch, "?")
+
+def substitute_deadkeys(m):
+    """Fill in dead keys.
+    
+    We configure a dead key for a character by adding AltGr to the matching keystroke.
+    Meaning that we cannot have dead keys on keys without AltGr
+    """
+    keys = list(map(klc_tochar, m.groups()[1:]))
+    assert(len(keys) == 5) # that is [normal, shift, ctrl, altgr, shift+altgr]
+    dk = [None, None, None, dklut.get(keys[0]), dklut.get(keys[1])]
+    for i in range(2):
+        if dk[i+3]: 
+            if keys[i+3] is not None:
+                raise ValueError('dead key configured for {} but AltGr combination already assigned to {}'.format(keys[i], keys[i+3]))
+            keys[i+3] = dk[i+3].deadkey
+    
+    s = m.group(1)
+    for i in range(5):
+        s += klc_fromchar(keys[i], dk[i] is not None) + '\t'
+    
+    s += '// '
+    s += ", ".join(map(klc_tocomment, keys))
+    s += "\n"
+    return s
+
+def get_deadkeytables():
+    t = []
     for d in dk:
-        print("DEADKEY\t{:04x} ".format(ord(d.deadkey)))
-        print()
+        t.append("DEADKEY\t{:04x} \n".format(ord(d.deadkey)))
+        t.append("\n")
         for a, b in d.all():
-            print("{:04x}\t{:04x}\t// {} -> {}".format(ord(a), ord(b), a, b))
-        print()
-elif what == "md":
+            t.append("{:04x}\t{:04x}\t// {} -> {}\n".format(ord(a), ord(b), a, b))
+        t.append("\n")
+    return t
+    
+def get_deadkeynames():
+    t = []
+    for d in dk:
+        ch = d.deadkey
+        t.append("{:04x} \"{}\"\n".format(ord(ch), unicodedata.name(ch)))
+    return t
+    
+# loop over lines, patch up the dead keys in the LAYOUT section
+lines = []
+with (open('qucmp.klc.in', encoding='utf-16')) as f:
+    layout = False
+    pat = re.compile(r'([0-9a-f]{2}\t+\w+\t+\w+\t+)(\S+)\t+(\S+)\t+(\S+)\t+(\S+)\t+(\S+)\t+//')
+    for line in f:
+        if line == "{layout}\n":
+            layout = True
+        elif line == "{endlayout}\n":
+            layout = False
+        elif line == "{deadkeys}\n":
+            lines += get_deadkeytables()
+        elif line == "{deadkeynames}\n":
+            lines += get_deadkeynames()
+        elif layout and line[0] != '5': # the '5' avoids modifying the "decimal separator" key
+            m = pat.match(line)
+            if m:
+                lines.append(substitute_deadkeys(m))
+            else:
+                raise ValueError("don’t know how to parse input line\n"+line)
+        else:
+            lines.append(line.format(v=version))
+
+            
+with (open('qucmp{}{}{}.klc'.format(*version), 'w', encoding='utf-16')) as f:
+    for l in lines:
+        f.write(l)
+    
+# write markdown
+
+with (open('keys.md', 'w', encoding='utf-8')) as f:
+    print("""# Keys
+
+## Combinations with dead keys
+""", file=f)
     for d in dk:
         if not d.extras: continue
-        print("### {}: <kbd>{}</kbd>".format(d.name, d.keypress))
-        print("| base | char |")
-        print("| ---- | ---- |")
+        print("### {}: <kbd>{}</kbd>".format(d.name, d.keypress), file=f)
+        print("| base | char | name     |", file=f)
+        print("| ---- | ---- | -------- |", file=f)
         for a, b in d.extras:
             if a == " ": a = "Space"
-            print("| <kbd>{}</kbd>  | {} |".format(a, b))
-        print()
-else:
-    print("Choose a format ('md' or 'klc') for printing the list of dead keys")
+            print("| <kbd>{}</kbd>  | {} | {} |".format(a, b, unicodedata.name(b).lower()), file=f)
+        print(file=f)
